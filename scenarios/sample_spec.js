877b7909f94e96dc6900c2860c6cdfba4cecb95b
kobabasu <keiji.kobayashi.web@gmail.com>
2015-07-09 06:07:24 +0900

    angularjsを無視する設定を追加

--- scenarios/sample_spec.js
+++ scenarios/sample_spec.js
@@ -1,5 +1,8 @@
 describe('サンプルテスト', function() {
+  browser.ignoreSynchronization = true;
+
   it('GoogleのタイトルはGoogle', function() {
+
     browser.get('http://www.google.com/');
 
     expect(browser.getTitle()).toEqual('Google');
